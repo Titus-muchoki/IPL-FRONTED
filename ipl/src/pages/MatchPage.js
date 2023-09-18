bf1f1c18d9b036e41c60c30f17c876eb29d010ae
kajela <titoyut56@gmail.com>
2023-09-18 11:40:54 +0300

    add the initial files

---
+++ ipl/src/pages/MatchPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MatchDetailCard from '../components/MatchDetailCard'
+
+export const MatchPage = () => {
+
+  return (
+    <div className='MatchPage'>
+        <h1>Match Card</h1>
+    </div>
+  )
+}
+
+export default MatchPage
