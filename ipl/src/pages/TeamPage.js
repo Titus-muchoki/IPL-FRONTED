2609672ea51bf225a962c5bbe6ddb50874b360c4
kajela <titoyut56@gmail.com>
2023-09-18 19:10:02 +0300

    add the initial fies

--- ipl/src/pages/TeamPage.js
+++ ipl/src/pages/TeamPage.js
@@ -5,6 +5,7 @@ import {React, useEffect, useState} from 'react'
 import MatchDetailCard from '../components/MatchDetailCard'
 import { MatchSmallCard } from '../components/MatchSmallCard'
 import { useParams } from 'react-router-dom';
+ import './TeamPage.scss';
 
 export const TeamPage = () => {
 
