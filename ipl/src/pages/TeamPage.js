9e34daf0fd139b2aabf6030bae32133bf90ebaf8
kajela <titoyut56@gmail.com>
2023-09-18 19:18:36 +0300

    add the initial files

--- ipl/src/pages/TeamPage.js
+++ ipl/src/pages/TeamPage.js
@@ -1,41 +1,40 @@
+import { React, useEffect, useState } from "react";
 
-import {React, useEffect, useState} from 'react'
-
-
-import MatchDetailCard from '../components/MatchDetailCard'
-import { MatchSmallCard } from '../components/MatchSmallCard'
-import { useParams } from 'react-router-dom';
- import './TeamPage.scss';
+import MatchDetailCard from "../components/MatchDetailCard";
+import { MatchSmallCard } from "../components/MatchSmallCard";
+import { useParams } from "react-router-dom";
+import "./TeamPage.scss";
 
 export const TeamPage = () => {
-
-    const[team, setTeam] = useState({matches: []});
-    const { teamName } = useParams()
-   useEffect(() => {
-     
+  const [team, setTeam] = useState({ matches: [] });
+  const { teamName } = useParams();
+  useEffect(() => {
     const fetchMatches = async () => {
-        const response = await fetch(`http://localhost:8080/team/${teamName}`)
-        const data = await response.json();
-        setTeam(data);        
+      const response = await fetch(`http://localhost:8080/team/${teamName}`);
+      const data = await response.json();
+      setTeam(data);
     };
 
-
     fetchMatches();
+  }, [teamName]);
 
-   }, [teamName]);
-   
-   if(!team || !team.teamName){
-    return <h1>Team not found</h1>
-   }
+  if (!team || !team.teamName) {
+    return <h1>Team not found</h1>;
+  }
 
-    return (
-        <div className='TeamPage'>
-            <h1>{team.teamName}</h1>
+  return (
+    <div className="TeamPage">
+      <div>
+        <h1>{team.teamName}</h1>
+      </div>
+      <div>Wins / Losses</div>
 
-             <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
-            {team.matches.slice(1).map( match => <MatchSmallCard teamName={team.teamName} match={match} />)}
-
-        </div>
-    )
-}
-
+      <div>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      </div>
+      {team.matches.slice(1).map((match) => (
+        <MatchSmallCard teamName={team.teamName} match={match} />
+      ))}
+    </div>
+  );
+};
