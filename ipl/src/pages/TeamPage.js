dfd8a608ea6f1f1674d3cd5435ae8d89bc6c4a88
kajela <titoyut56@gmail.com>
2023-09-19 10:41:58 +0300

    add the initial files

--- ipl/src/pages/TeamPage.js
+++ ipl/src/pages/TeamPage.js
@@ -4,43 +4,52 @@ import MatchDetailCard from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 import { useParams } from "react-router-dom";
 import "./TeamPage.scss";
+import { PieChart } from "react-minimal-pie-chart";
 
 export const TeamPage = () => {
-    const [team, setTeam] = useState({ matches: [] });
-    const { teamName } = useParams();
-    useEffect(() => {
-        const fetchMatches = async () => {
-            const response = await fetch(`http://localhost:8080/team/${teamName}`);
-            const data = await response.json();
-            setTeam(data);
-        };
+  const [team, setTeam] = useState({ matches: [] });
+  const { teamName } = useParams();
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch(`http://localhost:8080/team/${teamName}`);
+      const data = await response.json();
+      setTeam(data);
+    };
 
-        fetchMatches();
-    }, [teamName]);
+    fetchMatches();
+  }, [teamName]);
 
-    if (!team || !team.teamName) {
-        return <h1>Team not found</h1>;
-    }
+  if (!team || !team.teamName) {
+    return <h1>Team not found</h1>;
+  }
 
-    return (
-        <div className="TeamPage">
-            <div className="team-name-section">
-                <h1 className="team-name">{team.teamName}</h1>
-            </div>
-            <div className="win-loss-section">
-                Wins / Losses
-            </div>
+  return (
+    <div className="TeamPage">
+      <div className="team-name-section">
+        <h1 className="team-name">{team.teamName}</h1>
+      </div>
+      <div className="win-loss-section">
+        Wins / Losses
+        <PieChart
+          data={[
+            { title: "One", value: 10, color: "#E38627" },
+            { title: "Two", value: 15, color: "#C13C37" },
+            { title: "Three", value: 20, color: "#6A2135" },
+          ]}
+        />
+        ;
+      </div>
 
-            <div className="match-detail-section">
-                <h3>Latest Matches</h3>
-                <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
-            </div>
-            {team.matches.slice(1).map((match) => (
-                <MatchSmallCard teamName={team.teamName} match={match} />
-            ))}
-            <div className="more-link">
-                <a href="#">More</a>
-            </div>
-        </div>
-    );
+      <div className="match-detail-section">
+        <h3>Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      </div>
+      {team.matches.slice(1).map((match) => (
+        <MatchSmallCard teamName={team.teamName} match={match} />
+      ))}
+      <div className="more-link">
+        <a href="#">More ></a>
+      </div>
+    </div>
+  );
 };
