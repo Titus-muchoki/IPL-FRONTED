2d870db6b2aca42831815576766ecd88e2cea4d6
kajela <titoyut56@gmail.com>
2023-09-19 10:05:31 +0300

    add the initial files

--- ipl/src/components/MatchDetailCard.js
+++ ipl/src/components/MatchDetailCard.js
@@ -8,7 +8,9 @@ export const MatchDetailCard = ({ teamName, match }) => {
   const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
 
   const otherTeamRoute = `/teams/${otherTeam}`
+  
   const isMatchWon = teamName === match.matchWinner;
+
   return (
     <div className={isMatchWon ? 'MatchDetailCard won-card' : 'MatchDetailCard lost-card'}>
       <div>
--- ipl/src/components/MatchSmallCard.js
+++ ipl/src/components/MatchSmallCard.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-export const  MatchSmallCard = ({match, teamName}) => {
+export const MatchSmallCard = ({ match, teamName }) => {
 
-  const otherTeam = match.team1 === teamName ?  match.team2 : match.team1;
-  if(!match) return null;
+  const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
+  if (!match) return null;
 
   const otherTeamRoute = `/teams/${otherTeam}`
+
+  const isMatchWon = teamName === match.matchWinner;
+
   return (
-    <div className='MatchSmallCard'>
+    <div className={isMatchWon ? 'MatchSmallCard won-card' : 'MatchSmallCard lost-card'}>
       <h3><Link to={otherTeamRoute}>  {otherTeam}</Link></h3>
-        <p>{match.matchWinner} won by {match.resultMargin} {match.result}</p>
+      <p>{match.matchWinner} won by {match.resultMargin} {match.result}</p>
 
     </div>
   )
