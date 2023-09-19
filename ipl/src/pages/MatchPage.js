aaadfb05398e4023acd09088da147edb0029ed1e
kajela <titoyut56@gmail.com>
2023-09-19 20:28:23 +0300

    add the initial files

--- ipl/src/pages/MatchPage.js
+++ ipl/src/pages/MatchPage.js
@@ -1,37 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import MatchDetailCard from '../components/MatchDetailCard';
+
+
+import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { MatchDetailCard } from '../components/MatchDetailCard';
+
+import { YearSelector } from '../components/YearSelector';
+
+import './MatchPage.scss';
 
 export const MatchPage = () => {
-  const [matches, setMatches] = useState([]);
-  const { teamName, year } = useParams();
 
-  useEffect(() => {
-    const fetchMatches = async () => {
-      try {
-        const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
-        if (response.ok) {
-          const data = await response.json();
-          setMatches(data);
-        } else {
-          // Handle error if response is not ok (e.g., display an error message)
-          console.error('Failed to fetch matches');
-        }
-      } catch (error) {
-        console.error('An error occurred while fetching matches:', error);
-      }
-    };
 
-    fetchMatches();
-  }, [teamName, year]); // Specify dependencies here
+    const [matches, setMatches] = useState([]);
+    const { teamName, year } = useParams();
+    useEffect(
+        () => {
+         const fetchMatches = async () => {
+            const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data = await response.json();
+            setMatches(data);
 
-  return (
-    <div className='MatchPage'>
-      <h1>Match Card</h1>
-      {matches.map(match => (
-        <MatchDetailCard key={match.id} teamName={teamName} match={match} />
-      ))}
-    </div>
-  );
-};
+         };
+         fetchMatches();
+            
+
+
+        }, [teamName, year]
+    );
+
+   
+    return (
+        <div className="MatchPage">
+            <div className="year-selector">
+                <h3> Select Year </h3>
+                <YearSelector teamName={teamName} />
+            </div>
+            <div>
+                <h1 className="page-heading">{teamName} matches in {year}</h1>
+                {
+                    matches.map(match => <MatchDetailCard key={match.id} teamName={teamName} match={match} />)
+                }
+            </div>
+
+        </div>
+    );
+}
+
  export default MatchPage
